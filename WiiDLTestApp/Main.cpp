dd16ce6803b30fe6385bce60697578af04e2a4ac
SciFi <scifi@brettnet.info>
2009-06-03 22:48:18 +0100

    Added new unmanaged test app, though it doesn't do anything yet

---
+++ WiiDLTestApp/Main.cpp
@@ -0,0 +1,14 @@
+#include "Main.h"
+
+Main::Main(void)
+{
+}
+
+Main::~Main(void)
+{
+}
+
+void Main::Test()
+{
+	
+}
---
+++ WiiDLTestApp/Main.h
@@ -0,0 +1,10 @@
+#pragma once
+
+
+class Main
+{
+public:
+	Main(void);
+	~Main(void);
+	void Test();
+};
